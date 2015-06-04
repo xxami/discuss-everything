a7ea5628e0ae7477a0267a5fb2b74e838268ff8e
ami <ami@lilah.cat>
2015-06-04 17:17:56 +0100

    added generic html object plus some helpers, started work on the landing page

--- index.php
+++ index.php
@@ -3,8 +3,45 @@
 namespace de {
 
 	require_once('db.php');
+	require_once('lib/html.php');
 
-	echo 'test';
+	/**
+	 * check for cookie to see if this user has a temp account
+	 */
+
+	// todo
+
+	/**
+	 * create new post
+	 */
+	$button = html('button')
+		->attr(['name' => 'create_post'])
+		->val('create new post');
+	$form = html('form')
+		->attr(['action' => '', 'method' => 'post'])
+		->add_child($button);
+
+	/**
+	 * create the post
+	 */
+	if (isset($_POST[$button->attr('name')])) {
+
+		/**
+		 * todo: redirect user to created post
+		 */
+		echo 'todo: redirect';
+	}
+
+	/**
+	 * display the post button
+	 */
+	echo $form->render();
+
+	/**
+	 * display posts
+	 */
+
+	// todo
 
 }
 
--- lib/html.php
+++ lib/html.php
@@ -20,8 +20,77 @@ namespace de {
 		 */
 		public function add_child($entity) {
 			array_push($this->children, $entity);
+			return $this;
 		}
 
+	}
+
+	class HTMLElement extends HTMLEntity {
+
+		public $attributes = [];
+		public $val = null;
+		public $name = null;
+		public $closed = false;
+
+		public function __construct($name) {
+			$this->name = $name;
+			return $this;
+		}
+
+		public function attr($attrs) {
+			if (is_string($attrs)) {
+				/**
+				 * get prop
+				 */
+				return $this->attributes[$attrs];
+			}
+			/**
+			 * set props
+			 */
+			foreach ($attrs as $k => $v) {
+				$this->attributes[$k] = $v;
+			}
+			return $this;
+		}
+
+		public function val($val) {
+			$this->val = $val;
+			return $this;
+		}
+
+		public function toggle_closed() {
+			$this->closed = !$this->closed;
+		}
+
+		public function render() {
+			$res = '<' . $this->name;
+
+			foreach ($this->attributes as $k => $v) {
+				if (!$v) {
+					$res .= ' ' . $k;
+				}
+				else {
+					$res .= ' ' . $k . '="' . $v . '"';
+				}
+			}
+
+			$res .= '>';
+
+			foreach ($this->children as $child) {
+				$res .= $child->render();
+			}
+
+			if ($this->val) $res .= $this->val;
+
+			if (!$this->closed) $res .= '</' . $this->name . '>';
+
+			return $res;
+		}
+		
+	}
+
+	function html($name) {
+		return new HTMLElement($name);
 	}
 
 	class Form extends HTMLEntity {
