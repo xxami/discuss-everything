78bee61e6762b56026fcde3b6fb52692de9f7e9e
ami <ami@lilah.cat>
2015-06-04 16:12:30 +0100

    added prototype base sql

--- setup/core.sql
+++ setup/core.sql
@@ -3,8 +3,85 @@
  * core sql structure for discuss-everything
  */
 
-DROP TABLE IF EXISTS `test`;
-CREATE TABLE IF NOT EXISTS `test` (
-  `text` varchar(50) NOT NULL,
-  `cats` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS post;
+CREATE TABLE IF NOT EXISTS post (
+id bigint(20) NOT NULL,
+  title varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  url varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `type` int(11) NOT NULL,
+  content text COLLATE utf8_unicode_ci NOT NULL,
+  channel_1 int(11) NOT NULL,
+  channel_2 int(11) DEFAULT NULL,
+  channel_3 int(11) DEFAULT NULL,
+  tag_1 int(11) NOT NULL,
+  tag_2 int(11) DEFAULT NULL,
+  tag_3 int(11) DEFAULT NULL,
+  tag_4 int(11) DEFAULT NULL,
+  tag_5 int(11) DEFAULT NULL,
+  tag_6 int(11) DEFAULT NULL,
+  `user` int(11) NOT NULL,
+  posted datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS post_channel;
+CREATE TABLE IF NOT EXISTS post_channel (
+id int(11) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO post_channel VALUES(1, 'news');
+INSERT INTO post_channel VALUES(2, 'media');
+INSERT INTO post_channel VALUES(3, 'tech');
+INSERT INTO post_channel VALUES(4, 'gaming');
+INSERT INTO post_channel VALUES(5, 'fun');
+
+DROP TABLE IF EXISTS post_tag;
+CREATE TABLE IF NOT EXISTS post_tag (
+id int(11) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS post_type;
+CREATE TABLE IF NOT EXISTS post_type (
+id int(11) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO post_type VALUES(1, 'ask');
+INSERT INTO post_type VALUES(2, 'debate');
+INSERT INTO post_type VALUES(3, 'casual');
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE IF NOT EXISTS `user` (
+id int(11) NOT NULL,
+  `name` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT 'anonymous',
+  cookie_id varchar(32) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+ALTER TABLE post
+ ADD PRIMARY KEY (id);
+
+ALTER TABLE post_channel
+ ADD PRIMARY KEY (id);
+
+ALTER TABLE post_tag
+ ADD PRIMARY KEY (id);
+
+ALTER TABLE post_type
+ ADD PRIMARY KEY (id);
+
+ALTER TABLE user
+ ADD PRIMARY KEY (id);
+
+
+ALTER TABLE post
+MODIFY id bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE post_channel
+MODIFY id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+ALTER TABLE post_tag
+MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE post_type
+MODIFY id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+ALTER TABLE user
+MODIFY id int(11) NOT NULL AUTO_INCREMENT;
