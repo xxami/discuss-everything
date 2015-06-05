07326f432d1e10dea268c6c95bbfa6c57070f779
ami <ami@lilah.cat>
2015-06-05 16:31:29 +0100

    added post form, made some progress on adding posts to db, validation, etc. small changes

--- post.php
+++ post.php
@@ -65,7 +65,7 @@ namespace de {
 			<label>todo: login/register with email</label><br>
 			<label>login with a temporary account with username only 
 			(note: can be upgraded later)</label><br>
-			<input name="temp_user" maxlength="64" placeholder="Anonymous">
+			<input name="temp_user" maxlength="64" placeholder="your user name">
 			<button name="temp_login" type="submit">login</button>
 		</form>
 		<?php
@@ -80,10 +80,114 @@ namespace de {
 	 * display create new post if user logged in - todo: g+/email login support
 	 */
 	if (isset($_COOKIE['user_cookie_id'])) {
-		$r = query("select user.name from user where user.cookie_id = '?'",
+
+		$r = query("select user.id, user.name from user where user.cookie_id = '?'",
 			$_COOKIE['user_cookie_id']);
 		if ($user = $r->get_first()) {
+
+			if (isset($_POST['post_url']) && isset($_POST['post_title']) &&
+				isset($_POST['post_type']) && isset($_POST['post_content']) &&
+				isset($_POST['post_category']) && isset($_POST['post_tags'])) {
+
+				/**
+				 * form posted - put to db
+				 */
+
+				$tags = explode(' ', $_POST['post_tags']);
+				$invalid_tags = false;
+
+				foreach ($tags as $tag) {
+					$len = strlen($tag);
+					if ($len == 0 || $len > 32) $invalid_tags = true;
+				}
+
+				if (count($tags) > 6 || $invalid_Tags) {
+					/**
+					 * should never happen - js should prevent this
+					 */
+					echo 'sorry - invalid tags!';
+				}
+				elseif (!['ask' => 1, 'debate' => 1, 'casual' => 1][$_POST['post_type']]) {
+					/**
+					 * should never happen - js should prevent this
+					 */
+					echo 'sorry - invalid type!';
+				}
+				elseif (!['news' => 1, 'media' => 1,
+					'tech' => 1, 'gaming' => 1, 'fun' => 1][$_POST['post_category']]) {
+					/**
+					 * should never happen - js should prevent this
+					 */
+					echo 'sorry - invalid category!';
+				}
+				else if (strlen($_POST['post_url']) < 10 ||
+					strlen($_POST['post_title']) < 3 || strlen($_POST['post_content']) < 10) {
+					/**
+					 * should never happen - js should prevent this
+					 */
+					echo 'sorry - missing or not enough text in url, title or content!';
+				}
+				else {
+					/**
+					 * ok to post
+					 */
+
+					/**
+					 * get type ids from given type name
+					 */
+					$r = query("select * from post_type where post_type.name = '?';",
+						$_POST['post_type']);
+
+					/**
+					 * get category ids from given category name
+					 */
+					$r = query("select * from post_channel where post_channel.name = '?';",
+						$_POST['post_category']);
+
+					echo 'yay';
+				}
+
+				/**
+				 * end of page
+				 */
+				exit(0);
+			}
+
 			echo 'hello ' . $user['name'] . '!';
+
+			/**
+			 * post form
+			 */
+			?>
+
+			<form action="" method="post">
+				<label>url</label>
+				<input name="post_url" maxlength="256" placeholder="topic url"><br>
+				<label>title</label>
+				<input name="post_title" maxlength="128" placeholder="title"><br>
+				<label>type of post</label>
+				<input name="post_type" type="radio" value="ask">ask
+				<input name="post_type" type="radio" value="debate">discuss
+				<input name="post_type" type="radio" value="casual">casual<br>
+				<label>content</label>
+				<textarea name="post_content" cols="81" rows="15" placeholder="content"></textarea>
+				<br>
+				<label>category of post</label>
+				<select name="post_category">
+					<option value="news">news</option>
+					<option value="media">media</option>
+					<option value="tech">tech</option>
+					<option value="gaming">gaming</option>
+					<option value="fun">fun</option>
+				</select><br>
+				<label>post tags</label>
+				<input name="post_tags" maxlength="256" placeholder="tags seperated by space">
+				<br>
+				<br>
+				<button type="submit">create post!</button>
+			</form>
+
+		<?php
 		}
 		else {
 			/**
--- setup/core.sql
+++ setup/core.sql
@@ -10,15 +10,6 @@ id bigint(20) NOT NULL,
   url varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `type` int(11) NOT NULL,
   content text COLLATE utf8_unicode_ci NOT NULL,
-  channel_1 int(11) NOT NULL,
-  channel_2 int(11) DEFAULT NULL,
-  channel_3 int(11) DEFAULT NULL,
-  tag_1 int(11) NOT NULL,
-  tag_2 int(11) DEFAULT NULL,
-  tag_3 int(11) DEFAULT NULL,
-  tag_4 int(11) DEFAULT NULL,
-  tag_5 int(11) DEFAULT NULL,
-  tag_6 int(11) DEFAULT NULL,
   `user` int(11) NOT NULL,
   posted datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -34,6 +25,20 @@ INSERT INTO post_channel VALUES(2, 'media');
 INSERT INTO post_channel VALUES(3, 'tech');
 INSERT INTO post_channel VALUES(4, 'gaming');
 INSERT INTO post_channel VALUES(5, 'fun');
+
+DROP TABLE IF EXISTS post_has_channel;
+CREATE TABLE IF NOT EXISTS post_has_channel (
+id bigint(20) NOT NULL,
+  post_id bigint(20) NOT NULL,
+  post_channel_id int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS post_has_tag;
+CREATE TABLE IF NOT EXISTS post_has_tag (
+id bigint(20) NOT NULL,
+  post_id bigint(20) NOT NULL,
+  post_tag_id int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS post_tag;
 CREATE TABLE IF NOT EXISTS post_tag (
@@ -51,18 +56,30 @@ INSERT INTO post_type VALUES(1, 'ask');
 INSERT INTO post_type VALUES(2, 'debate');
 INSERT INTO post_type VALUES(3, 'casual');
 
+DROP TABLE IF EXISTS test;
+CREATE TABLE IF NOT EXISTS test (
+  `text` varchar(50) NOT NULL,
+  cats int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 DROP TABLE IF EXISTS user;
 CREATE TABLE IF NOT EXISTS `user` (
 id int(11) NOT NULL,
-  `name` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT 'anonymous',
-  cookie_id varchar(32) CHARACTER SET latin1 DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `name` varchar(64) CHARACTER SET utf8 NOT NULL DEFAULT 'anonymous',
+  cookie_id varchar(32) CHARACTER SET utf8 DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 ALTER TABLE post
  ADD PRIMARY KEY (id);
 
 ALTER TABLE post_channel
+ ADD PRIMARY KEY (id);
+
+ALTER TABLE post_has_channel
+ ADD PRIMARY KEY (id), ADD UNIQUE KEY id (id);
+
+ALTER TABLE post_has_tag
  ADD PRIMARY KEY (id);
 
 ALTER TABLE post_tag
@@ -79,9 +96,13 @@ ALTER TABLE post
 MODIFY id bigint(20) NOT NULL AUTO_INCREMENT;
 ALTER TABLE post_channel
 MODIFY id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+ALTER TABLE post_has_channel
+MODIFY id bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE post_has_tag
+MODIFY id bigint(20) NOT NULL AUTO_INCREMENT;
 ALTER TABLE post_tag
 MODIFY id int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE post_type
 MODIFY id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 ALTER TABLE user
-MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+MODIFY id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
